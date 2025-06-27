735918f9ed4ce186d8a183c4c66552aecf1e2f19
Ismenia Cristina <snowismscris@gmail.com>
2025-06-27 16:05:20 -0300

    relacionando componente com a área administrativa

--- frontend/src/pages/Administração.js
+++ frontend/src/pages/Administração.js
@@ -146,7 +146,11 @@ export default function UsuariosAdmin() {
 									>
 										<Edit size={20} />
 									</button>
-									<button className="text-red-600 hover:text-red-800">
+									<button className="text-red-600 hover:text-red-800"
+									onClick={() => {onDelete(userId)
+										setMenuOpen(false)
+									}}
+									>
 										<Trash size={20} />
 									</button>
 								</div>
